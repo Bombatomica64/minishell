8db6bf8364f998f7768ea6a4edf402d194aa63ce
Bombatomica_64 <qualcosa2001@gmail.com>
2024-02-08 18:26:53 +0100

    Update header file imports

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:05:20 by mruggier          #+#    #+#             */
-/*   Updated: 2024/02/08 18:22:18 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/08 18:26:50 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <errno.h>
-# include "utils/utils.h"
+# include "./libft/libft.h"
 # include "./pipex/pipex.h"
+# include "./utils/utils.h"
 
 typedef enum e_error
 {

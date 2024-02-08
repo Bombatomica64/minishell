f4a4cd002cae26cd6ada53ae0b940a42a9bafca7
marcor2742 <marcoruggiero.mail@gmail.com>
2024-02-08 17:48:28 +0100

    header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 17:05:20 by mruggier          #+#    #+#             */
-/*   Updated: 2024/02/07 17:18:38 by mruggier         ###   ########.fr       */
+/*   Updated: 2024/02/08 17:47:44 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,18 @@
 # include <sys/wait.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+typedef struct s_pipex
+{
+	char	**cmd1;
+	char	**cmd2;
+	char	*path1;
+	char	*path2;
+	char	*filein;
+	char	*fileout;
+	int		fd_in;
+	int		fd_out;
+}				t_pipex;
 
 enum e_error
 {

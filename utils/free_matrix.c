581ca8bfece8079ff6b0fc4c68743980450811cc
DUPEgduranti <gduranti@student.42firenze.it>
2024-02-19 15:18:16 +0100

    Update free_matrix.c and remove prog_term.c

--- utils/free_matrix.c
+++ utils/free_matrix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_matrix.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 18:10:53 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/02/08 18:11:14 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/19 15:17:57 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,21 @@ void	free_matrix(char **matrix)
 		i++;
 	}
 	free(matrix);
+}
+
+void	ft_freenclose(t_data *data)
+{
+	int i;
+
+	i = -1;
+	while (++i < data->nb_total)
+	{
+		if (data->input[i].is_a_file == TRUE)
+			free(data->input[i].file.name);
+		else
+			free(data->input[i].cmd.name);
+	}
+	free_matrix(data->in_p.cmds);
+	free_matrix(data->in_p.files);
+	free(data->input);
 }

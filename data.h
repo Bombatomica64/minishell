ba8b26aae14b561d72df2aa891bf63a12b983dfa
DUPEgduranti <gduranti@student.42firenze.it>
2024-02-19 13:09:04 +0100

    Update data.h and utils/utils.h

--- data.h
+++ data.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   data.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 21:13:02 by marvin            #+#    #+#             */
-/*   Updated: 2024/02/19 11:59:51 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/19 13:06:35 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,5 @@
 # define DATA_H
 
 # include "utils/utils.h"
-
-typedef enum e_io
-{
-	INPUT = 0,// < file in input
-	HEREDOC,// << limiter, terminal input until limiter
-	TRUNC = O_TRUNC,// > file, rewrites the lines in  the output file
-	APPEND = O_APPEND// >> file, add more lines in the output file
-}	t_io;
-
-typedef struct s_file
-{
-	int		fd;
-	t_io	type;
-	char	*name; // file name without the type (> >> < << UwU)
-}	t_file;
-
-typedef struct s_cmd
-{
-	char	*name;
-	char	*path;
-}	t_cmd;
-
-typedef struct s_input
-{
-	//int		i; // position of the command or file [1: grep e] {2: < file 2}
-	t_cmd	cmd; // command, set to NULL if it's a file
-	t_file	file; // file, set to NULL if it's a command
-	t_bool	is_a_file; // true if it's a file, false if it's a command
-}	t_input;
-
-typedef struct s_pipex_data
-{
-	char	**cmds; //name
-	char	**files; //name
-	int		fds[2]; // 2 fd (i/o)
-}	t_pi_d;
-
-typedef struct s_data
-{
-	int		nb_total; // total number of commands and files in input
-	int		nb_files; // number of files
-	int		nb_cmds; // number of commands
-	int		fd_in; // input file descriptor
-	int		fd_out; // output file descriptor
-	int		original_stdin;
-	int		original_stdout;
-	t_bool	input_found; // true if there is an input file, false if there isn't
-	t_pi_d	in_p; // pipex data
-	t_input	*input; // array of commands and files
-}	t_data;
 
 #endif
--- utils/utils.h
+++ utils/utils.h
@@ -6,7 +6,7 @@
 /*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 18:11:21 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/02/19 12:15:59 by gduranti         ###   ########.fr       */
+/*   Updated: 2024/02/19 13:06:47 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,59 @@ typedef enum e_error
 	OPEN
 }	t_error;
 
+typedef enum e_io
+{
+	INPUT = 0,// < file in input
+	HEREDOC,// << limiter, terminal input until limiter
+	TRUNC = O_TRUNC,// > file, rewrites the lines in  the output file
+	APPEND = O_APPEND// >> file, add more lines in the output file
+}	t_io;
+
+typedef struct s_file
+{
+	int		fd;
+	t_io	type;
+	char	*name; // file name without the type (> >> < << UwU)
+}	t_file;
+
+typedef struct s_cmd
+{
+	char	*name;
+	char	*path;
+}	t_cmd;
+
+typedef struct s_input
+{
+	//int		i; // position of the command or file [1: grep e] {2: < file 2}
+	t_cmd	cmd; // command, set to NULL if it's a file
+	t_file	file; // file, set to NULL if it's a command
+	t_bool	is_a_file; // true if it's a file, false if it's a command
+}	t_input;
+
+typedef struct s_pipex_data
+{
+	char	**cmds; //name
+	char	**files; //name
+	int		fds[2]; // 2 fd (i/o)
+}	t_pi_d;
+
+typedef struct s_data
+{
+	int		nb_total; // total number of commands and files in input
+	int		nb_files; // number of files
+	int		nb_cmds; // number of commands
+	int		fd_in; // input file descriptor
+	int		fd_out; // output file descriptor
+	int		original_stdin;
+	int		original_stdout;
+	t_bool	input_found; // true if there is an input file, false if there isn't
+	t_pi_d	in_p; // pipex data
+	t_input	*input; // array of commands and files
+}	t_data;
+
 void	free_matrix(char **matrix);
 void	ft_error(const char *str, t_error error);
 t_bool	ft_malloc_err(void *ptr, char *str);
+void	ft_freenclose(t_data *data);
 
 #endif

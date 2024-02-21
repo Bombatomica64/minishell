19dddfa7d4e034167270323361c237cdcc99ae2d
DUPEgduranti <gduranti@student.42firenze.it>
2024-02-21 10:02:39 +0100

    Refactor heredoc functions and fix file handling

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -6,37 +6,37 @@
 /*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 16:32:21 by gduranti          #+#    #+#             */
-/*   Updated: 2024/02/21 09:52:58 by gduranti         ###   ########.fr       */
+/*   Updated: 2024/02/21 10:00:20 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	heredoc_open_write(char *str)
+void	heredoc_write(char *str, int fd)
 {
-	int		fd;
 	char	*line;
 
-	fd = open("heredoc.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd < 0)
-	{
-		printf("Error opening file\n");
-		return (NULL);
-	}
 	while (read(fd, line, 1024) != 0);
 	ft_putendl_fd(str, fd);
-	close(fd);
 }
 
 void	heredoc_creat(char *limiter)
 {
 	char	*str;
+	int		fd;
 
 	str = NULL;
+	fd = open(".heredoc.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+	{
+		printf("Error opening file\n");
+		return ;
+	}
 	while (ft_strncmp((const char *)str, (const char *)limiter, ft_strlen(str) != 0))
 	{
 		str = readline("heredoc> ");
 		if (ft_strncmp((const char *)str, (const char *)limiter, ft_strlen(str) != 0))
-			heredoc_open_write(str);
+			heredoc_write(str, fd);
 	}
+	close(fd);
 }

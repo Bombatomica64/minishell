9b415e36a4fbf1ee1fee53348d247330005c81e9
DOPEgduranti <150932393+DOPEgduranti@users.noreply.github.com>
2024-02-21 11:02:30 +0100

    Heregu (#21)
    * Update heredoc functionality
    
    * Refactor heredoc functions and fix file handling

--- srcs/heredoc.c
+++ srcs/heredoc.c
@@ -3,42 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 16:32:21 by gduranti          #+#    #+#             */
-/*   Updated: 2024/02/20 18:11:19 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/21 10:48:44 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	heredoc_open_write(void)
+void	heredoc_write(char *str, int fd)
 {
-	int		fd;
+	char	*line;
 
-	fd = open("heredoc.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd < 0)
-	{
-		printf("Error opening file\n");
-		return (NULL);
-	}
+	while (read(fd, line, 1024) != 0);
+	ft_putendl_fd(str, fd);
 }
 
-void	heredoc_creat(char *limiter, t_data *data)
+void	heredoc_creat(char *limiter)
 {
 	char	*str;
 	int		fd;
 
-	fd = open("heredoctemp", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-	str = readline("heredoc> " );
-	heredoc_open_write(str);
-	while (ft_strcmp(str, limiter) != NULL)
+	str = NULL;
+	fd = open(".heredoc.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd < 0)
 	{
-		write(fd, str, ft_strlen(str));
-		free(str);
-		str = readline("heredoc> " );
+		printf("Error opening file\n");
+		return ;
+	}
+	while (ft_strncmp((const char *)str, (const char *)limiter, ft_strlen(str) != 0))
+	{
+		str = readline("heredoc> ");
+		if (ft_strncmp((const char *)str, (const char *)limiter, ft_strlen(str) != 0))
+			heredoc_write(str, fd);
 	}
 	free(str);
 	close(fd);
-	printf("sesso pazzo\n");
 }

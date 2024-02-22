b881232d76d29990bb3e7271e2e06617b4e1f0f5
sim1gariglio <simone.gariglio1@gmail.com>
2024-02-22 15:24:22 +0100

    Fix quote handling logic in quote_waiting function

--- srcs/quotehandler.c
+++ srcs/quotehandler.c
@@ -6,7 +6,7 @@
 /*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 11:27:46 by sgarigli          #+#    #+#             */
-/*   Updated: 2024/02/22 12:19:17 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/02/22 12:51:54 by sgarigli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,8 @@ void	quote_waiting(char **tmp, t_bool *quote, char *quote_type, t_type type)
 	while (*quote == TRUE)
 	{
 		*tmp = ft_strjoin(*tmp, readline("\033[0;34mquote> \033[0m"));
-	if (check_quote(*tmp, *quote_type, type, index) == TRUE)
-		*quote = FALSE;
+		if (check_quote(*tmp, *quote_type, type, index) == TRUE)
+			*quote = FALSE;
 	}
 }
 
@@ -63,18 +63,20 @@ t_bool	check_quote(char *tmp, char quote_type, t_type type, int index)
 	int	i;
 	int	quote_count;
 
-	quote_count = 1;
-	if (type == COMMAND || type == BUILT_IN)
+	(void)type;
+	(void)index;
+	quote_count = 0;
+	i = 0;
+	if (type != COMMAND && type != BUILT_IN)
 		quote_count++;
-	i = index;
 	while (tmp[i] != '\0')
 	{
-		if (tmp[i] == quote_type && quote_count % 2 == 1)
+		if (tmp[i] == quote_type)
 			quote_count++;
-		else if(tmp[i] == '\'' || tmp[i] == '\"')
+		else if (tmp[i] != quote_type && (tmp[i] == '\'' || tmp[i] == '\"') && quote_count % 2 == 0)
 		{
 			quote_type = tmp[i];
-			quote_count++;		
+			quote_count++;
 		}
 		i++;
 	}

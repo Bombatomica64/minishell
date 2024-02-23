45d773e32a1bed3f79b005285b59d514690b44ce
DUPEgduranti <gduranti@student.42firenze.it>
2024-02-23 10:43:59 +0100

    echo

--- built_in/echo.c
+++ built_in/echo.c
@@ -6,19 +6,23 @@
 /*   By: gduranti <gduranti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 15:24:10 by gduranti          #+#    #+#             */
-/*   Updated: 2024/02/22 15:24:16 by gduranti         ###   ########.fr       */
+/*   Updated: 2024/02/23 10:42:03 by gduranti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_echo(char *str, t_data *data)
-{
-	int	i;
+#include "built_in.h"
 
-	str += 4;
-	i = 0;
-	while (*str)
-	{
-		while (*str == ' ' || *str == '\t')
-			str++;
-	}
+t_bool	ft_echo(char **mtx)
+{
+	int i;
+
+	if (!mtx || ft_strcmp(mtx[0], "echo") != 0)
+		return (FALSE);
+	i = 1;
+	if (ft_strcmp(mtx[i], "-n") == TRUE)
+		i++;
+	ft_printmtx(&mtx[i]);
+	if (ft_strcmp(mtx[i], "-n") == TRUE)
+		ft_printf("\n");
+	return (TRUE);
 }

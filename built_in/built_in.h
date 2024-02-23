507100c281e485f2e6a74e2856f9720ab4398351
Bombatomica_64 <qualcosa2001@gmail.com>
2024-02-23 16:16:24 +0100

    Update built-in functions and fix bugs

--- built_in/built_in.h
+++ built_in/built_in.h
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 10:03:32 by gduranti          #+#    #+#             */
-/*   Updated: 2024/02/23 10:51:27 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/23 16:16:16 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,20 @@
 # include "../utils/utils.h"
 
 t_bool	ft_echo(char **mtx);
-void	print_pwd(char **envp);
 void	ft_cd(char *str, t_data *data);
 void	ft_env(char **envp);
+
+// unset & helper functions
+t_bool	ft_unset(char **mtx, char ***envp);
+void	remove_envp_entry(char ***envp, char *entry);
+
+// export & helper functions
+
+t_bool	ft_export(char ***envp, char *str);
+t_bool	add_to_env(char ***envp, char *str);
+t_bool	update_env(char ***envp, char *str);
+
+// debug functions
+void	print_pwds(char **envp);
 
 #endif
--- built_in/unset.c
+++ built_in/unset.c
@@ -6,16 +6,11 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 10:14:12 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/02/23 15:49:46 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/23 16:05:18 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built_in.h"
-
-void	ft_env(char **envp)
-{
-	print_matrix(envp);
-}
 
 void	remove_envp_entry(char ***envp, char *entry)
 {
@@ -59,33 +54,42 @@ t_bool	ft_unset(char **mtx, char ***envp)
 
 t_bool	export(char ***envp, char *str)
 {
+	if (!str)
+		return (FALSE);
+	if (find_in_env(*envp, str) != -1)
+		return (add_to_env(envp, str));
+	else
+		return (update_env(envp, str));
+}
+
+t_bool	add_to_env(char ***envp, char *str)
+{
 	int		i;
 	char	**new_envp;
 
 	i = 0;
-	if (!str)
+	while ((*envp)[i])
+		i++;
+	new_envp = ft_calloc(i + 2, sizeof(char *));
+	if (!new_envp)
 		return (FALSE);
-	if (find_in_env(*envp, str) != -1)
-	{
-		while ((*envp)[i])
-			i++;
-		new_envp = ft_calloc(i + 2, sizeof(char *));
-		if (!new_envp)
-			return (FALSE);
-		ft_memcpy(new_envp, *envp, i * sizeof(char *));
-		new_envp[i] = ft_strdup(str);
-		if (!new_envp[i])
-			return (FALSE);
-		free(*envp);
-		*envp = new_envp;
-	}
-	else
-	{
-		i = find_in_env(*envp, str);
-		free((*envp)[i]);
-		(*envp)[i] = ft_strdup(str);
-		if (!(*envp)[i])
-			return (FALSE);
-	}
+	ft_memcpy(new_envp, *envp, i * sizeof(char *));
+	new_envp[i] = ft_strdup(str);
+	if (!new_envp[i])
+		return (FALSE);
+	free(*envp);
+	*envp = new_envp;
+	return (TRUE);
+}
+
+t_bool	update_env(char ***envp, char *str)
+{
+	int		i;
+
+	i = find_in_env(*envp, str);
+	free((*envp)[i]);
+	(*envp)[i] = ft_strdup(str);
+	if (!(*envp)[i])
+		return (FALSE);
 	return (TRUE);
 }

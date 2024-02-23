5e19cbdec5040b97fbb3cbe9cadbcd2991ef8f30
Bombatomica_64 <qualcosa2001@gmail.com>
2024-02-23 12:30:28 +0100

    Update unset.c and matrix_utils.c

--- built_in/unset.c
+++ built_in/unset.c
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 10:14:12 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/02/23 11:35:31 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/23 12:26:05 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,25 @@ t_bool	ft_unset(char **mtx, char ***envp)
 		}
 		i++;
 	}
+	return (TRUE);
+}
+
+t_bool	export(char ***envp, char *str)
+{
+	int		i;
+	char	**new_envp;
+
+	i = 0;
+	while ((*envp)[i])
+		i++;
+	new_envp = ft_calloc(i + 2, sizeof(char *));
+	if (!new_envp)
+		return (FALSE);
+	ft_memcpy(new_envp, *envp, i * sizeof(char *));
+	new_envp[i] = ft_strdup(str);
+	if (!new_envp[i])
+		return (FALSE);
+	free(*envp);
+	*envp = new_envp;
 	return (TRUE);
 }

753e530a81604f3b1fc42ddc7f9ddc4c116996ff
Bombatomica_64 <qualcosa2001@gmail.com>
2024-02-28 11:53:13 +0100

    Update heredoc_creat function in heredoc.c

--- utils/heredoc.c
+++ utils/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 16:32:21 by gduranti          #+#    #+#             */
-/*   Updated: 2024/02/28 11:44:12 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/02/28 11:51:55 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int	heredoc_creat(char *limiter)
 	if (pid == 0)
 	{
 		str = readline("heredoc> ");
-		while (ft_strcmp(str, limiter, ft_strlen(limiter)) != 0)
+		while (ft_strcmp(str, limiter) != 0)
 		{
 			write(2, str, ft_strlen(str));
 			ft_putendl_fd_free(&str, fd[1]);

ebe236b01afb86bb430aab831640265d567767c2
Bombatomica_64 <qualcosa2001@gmail.com>
2024-03-01 15:17:21 +0100

    Add pipe_count function to lexer.c

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -6,11 +6,28 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:52:13 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/03/01 11:58:35 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/03/01 15:15:25 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
+
+int	pipe_count(char *line, t_data *data)
+{
+	int		i;
+	int		pipe_count;
+
+	i = 0;
+	pipe_count = 0;
+	while (line[i])
+	{
+		if (line[i] == '|')
+			pipe_count++;
+		i++;
+	}
+	data->pipe_nbr = pipe_count;
+	return (pipe_count);
+}
 
 t_bool	lexer_error(char *error, t_data *data)
 {

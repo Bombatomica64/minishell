83bd6daade60906ea17c65e34f27b2fdcc5a0240
Bombatomica_64 <qualcosa2001@gmail.com>
2024-03-12 17:48:41 +0100

    Update lexer.h and add lexer.c

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:52:36 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/03/11 17:40:39 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/03/12 17:33:40 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,5 @@ t_bool	lexer(char **line, t_data *data);
 t_bool	lexer_error(char *error, t_data *data, char c);
 
 #endif
+
+// Path: lexer/lexer.c

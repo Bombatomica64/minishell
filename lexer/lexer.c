94cc4ee200d460a31197e339b4ebc6f99e21496a
marcor2742 <marcoruggiero.mail@gmail.com>
2024-03-25 17:23:17 +0100

    lexer {}, []?

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgarigli <sgarigli@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mruggier <mruggier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:52:13 by lmicheli          #+#    #+#             */
-/*   Updated: 2024/03/25 10:10:22 by sgarigli         ###   ########.fr       */
+/*   Updated: 2024/03/25 17:15:23 by mruggier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,39 @@ t_bool	lexer_error(char *error, t_data *data, char c)
 	return (FALSE);
 }
 
+int	graffe_check(char *line, t_data *data)
+{
+	t_quote	squote;
+	int		i;
+	int		graffe;
+
+	squote = (t_quote){FALSE, 0};
+	i = 0;
+	graffe = 0;
+	while (line[i])
+	{
+		quote_start(&squote.open, line[i], &squote.type);
+		if (line[i] == '{' && squote.open == FALSE)
+			graffe++;
+		if (line[i] == '}' && squote.open == FALSE)
+			graffe--;
+		i++;
+	}
+	if (graffe < 0)
+		return (lexer_error("minishell: syntax error near unexpected token `", data, '}'));
+	if (graffe > 0)
+		return (lexer_error("minishell: syntax error near unexpected token `", data, '{'));
+	return (TRUE);
+}
+
 t_bool	lexer(char **line, t_data *data)
 {
 	if (ft_strlen(*line) == 0)
 		return (TRUE);
 	if (pipe_check(*line) == FALSE)
 		return (lexer_error("minishell: syntax error near unexpected token `", data, '|'));
+	if (graffe_check(*line, data) == FALSE)
+		return (FALSE);
 	pipe_count(*line, data);
 	quote_check(line);
 	if (redir_check(*line, data) == FALSE)

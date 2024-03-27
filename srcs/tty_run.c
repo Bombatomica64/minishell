2bd0edbef6f60b4c631d24a8acba20fa7bba2b80
Bombatomica_64 <qualcosa2001@gmail.com>
2024-03-27 18:44:11 +0100

    testare con gdb dove rimane aperta la pipe

--- srcs/tty_run.c
+++ srcs/tty_run.c
@@ -6,7 +6,7 @@
 /*   By: lmicheli <lmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 15:41:01 by gduranti          #+#    #+#             */
-/*   Updated: 2024/03/27 18:07:12 by lmicheli         ###   ########.fr       */
+/*   Updated: 2024/03/27 18:43:50 by lmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,3 +85,4 @@ void	ft_tty_exec(t_data *data)
 		process_input(data);
 	}
 }
+//ciao :)
